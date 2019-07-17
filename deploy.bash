69fdd142e15b397551da259944531b2bdc4078d1
v.shubenkov <v.shubenkov@corp.mail.ru>
2019-07-17 13:30:39 +0300

    База данных с процедурами insert, select и fix_nil_to_empty Процедура загрузки файла. Сохранение пути к нему в таблице

--- deploy.bash
+++ deploy.bash
@@ -1,9 +1,8 @@
 tarantoolctl stop mysql_db
 nginx -s stop
-cp mysql_db.lua /usr/local/Cellar/tarantool/1.10.2.1_2/share/tarantool
-cp mysql_app_db.lua /usr/local/etc/tarantool/instances.available/
+cp db_config/mysql_db.lua /usr/local/Cellar/tarantool/1.10.2.1_2/share/tarantool
+cp db_config/mysql_app_db.lua /usr/local/etc/tarantool/instances.available/
 cp nginx_wsgi.conf nginx_tarantool_for_Dummies.conf /usr/local/openresty/nginx/conf/
 rm -rf /usr/local/var/lib/tarantool/mysql_db/*
 tarantoolctl start mysql_db
 nginx
-
